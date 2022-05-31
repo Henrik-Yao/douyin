3492518eea1e5b0fc9d317092cbb43727d52aac7
younger <1908508603@qq.com>
2022-06-01 00:01:38 +0800

    video

---
+++ go/model/videoAll.go
@@ -0,0 +1,16 @@
+package model
+
+type Video struct {
+	ID            int64  `json:"id,omitempty"`
+	UserId        int64  `json:"user_id,omitempty"`
+	Name          string `json:"name,omitempty"`
+	FollowCount   int64  `json:"follow_count,omitempty"`
+	FollowerCount int64  `json:"follower_count,omitempty"`
+	IsFollow      bool   `json:"is_follow,omitempty"`
+	PlayUrl       string `json:"play_url" json:"play_url,omitempty"`
+	CoverUrl      string `json:"cover_url,omitempty"`
+	FavoriteCount int64  `json:"favorite_count,omitempty"`
+	CommentCount  int64  `json:"comment_count,omitempty"`
+	IsFavorite    bool   `json:"is_favorite,omitempty"`
+	Title         string `json:"title"`
+}
