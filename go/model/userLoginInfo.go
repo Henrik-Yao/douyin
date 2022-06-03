838bc44914106ed010142fb7a2b448d6b8e184a2
younger <1908508603@qq.com>
2022-06-04 01:48:20 +0800

    userLoginInfo

--- go/model/userLoginInfo.go
+++ go/model/userLoginInfo.go
@@ -10,3 +10,11 @@ type UserLoginInfo struct {
 	FollowerCount int64  `json:"follower_count,omitempty"`
 	IsFollow      bool   `json:"is_follow,omitempty"`
 }
+
+type UserLogin struct {
+	UserId        int64  `json:"user_id,omitempty"`
+	Name          string `json:"name,omitempty"`
+	FollowCount   int64  `json:"follow_count,omitempty"`
+	FollowerCount int64  `json:"follower_count,omitempty"`
+	IsFollow      bool   `json:"is_follow,omitempty"`
+}
