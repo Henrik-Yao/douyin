80ca293bf813abf057ebbc8970c512b1b115a628
younger <1908508603@qq.com>
2022-06-04 01:49:01 +0800

    video

--- go/model/videoAll.go
+++ go/model/videoAll.go
@@ -2,13 +2,20 @@ package model
 
 import "github.com/jinzhu/gorm"
 
-type Video struct {
+type Video struct { //视频信息
+	VideoId       int64     `json:"video_id,omitempty"`
+	Author        UserLogin `json:"author"`
+	PlayUrl       string    `json:"play_url" json:"play_url,omitempty"`
+	CoverUrl      string    `json:"cover_url,omitempty"`
+	FavoriteCount int64     `json:"favorite_count,omitempty"`
+	CommentCount  int64     `json:"comment_count,omitempty"`
+	IsFavorite    bool      `json:"is_favorite,omitempty"`
+	Title         string    `json:"title"`
+}
+
+type VideoNoAuthor struct {
 	gorm.Model
-	UserId        int64  `json:"user_id,omitempty"`
-	Name          string `json:"name,omitempty"`
-	FollowCount   int64  `json:"follow_count,omitempty"`
-	FollowerCount int64  `json:"follower_count,omitempty"`
-	IsFollow      bool   `json:"is_follow,omitempty"`
+	UserId        int64  `json:"user_id"`
 	PlayUrl       string `json:"play_url" json:"play_url,omitempty"`
 	CoverUrl      string `json:"cover_url,omitempty"`
 	FavoriteCount int64  `json:"favorite_count,omitempty"`
