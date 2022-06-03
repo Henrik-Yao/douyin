812b907e7be221d88ef196e4b0f80a023d4dcbd7
KEPADEDAIDAI <sunabrhh@foxmail.com>
2022-06-03 17:32:18 +0800

    修复了由于方法重名导致的无法运行，但是feed仍然只能用本地数据

--- go/controller/demo_data.go
+++ go/controller/demo_data.go
@@ -2,14 +2,14 @@ package controller
 
 import "douyin/go/model"
 
-var DemoVideos = []model.Video{
+var DemoVideos = []model.FeedVideo{
 	{
 		Id:      1,
 		Author:  DemoUserinfo,
 		PlayUrl: "http://" + "150.158.44.75" + ":" + "8080" + "/static/" + "bear.mp4",
 	},
 }
-var DemoUserinfo = model.UserInfo{
+var DemoUserinfo = model.Author{
 	Id:            1,
 	Name:          "ava",
 	FollowCount:   0,
---
+++ go/model/feedVideo.go
@@ -0,0 +1,11 @@
+package model
+
+type FeedVideo struct {
+	Id            int64  `json:"id,omitempty"`
+	Author        Author `json:"author"`
+	PlayUrl       string `json:"play_url" json:"play_url,omitempty"`
+	CoverUrl      string `json:"cover_url,omitempty"`
+	FavoriteCount int64  `json:"favorite_count,omitempty"`
+	CommentCount  int64  `json:"comment_count,omitempty"`
+	IsFavorite    bool   `json:"is_favorite,omitempty"`
+}
