b5a555388b17172f8ec7853582705762a9587bd2
younger <1908508603@qq.com>
2022-06-03 23:17:29 +0800

    videomodel

--- go/model/video.go
+++ go/model/video.go
@@ -1,11 +1,25 @@
 package model
 
+import "github.com/jinzhu/gorm"
+
 type Video struct { //视频信息
-	Id            int64    `json:"id,omitempty"`
-	Author        UserInfo `json:"author"`
-	PlayUrl       string   `json:"play_url" json:"play_url,omitempty"`
-	CoverUrl      string   `json:"cover_url,omitempty"`
-	FavoriteCount int64    `json:"favorite_count,omitempty"`
-	CommentCount  int64    `json:"comment_count,omitempty"`
-	IsFavorite    bool     `json:"is_favorite,omitempty"`
+	VideoId       int64     `json:"video_id,omitempty"`
+	Author        LoginUser `json:"author"`
+	PlayUrl       string    `json:"play_url" json:"play_url,omitempty"`
+	CoverUrl      string    `json:"cover_url,omitempty"`
+	FavoriteCount int64     `json:"favorite_count,omitempty"`
+	CommentCount  int64     `json:"comment_count,omitempty"`
+	IsFavorite    bool      `json:"is_favorite,omitempty"`
+	Title         string    `json:"title"`
+}
+
+type VideoNoAuthor struct {
+	gorm.Model
+	UserId        int64  `json:"user_id"`
+	PlayUrl       string `json:"play_url" json:"play_url,omitempty"`
+	CoverUrl      string `json:"cover_url,omitempty"`
+	FavoriteCount int64  `json:"favorite_count,omitempty"`
+	CommentCount  int64  `json:"comment_count,omitempty"`
+	IsFavorite    bool   `json:"is_favorite,omitempty"`
+	Title         string `json:"title"`
 }
