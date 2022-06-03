2157922ed367bf13ee4edbfdf8ed35c8f9d8d8ad
LIUCJ <593562829@qq.com>
2022-06-03 19:47:07 +0800

    model/relation的修改

--- go/model/relation.go
+++ go/model/relation.go
@@ -1,6 +1,11 @@
 package model
 
+import (
+	"github.com/jinzhu/gorm"
+)
+
 type Relation struct {
+	gorm.Model
 	UserId   int64 `json:"user_id,omitempty"`
 	ToUserId int64 `json:"to_user_id,omitempty"`
 }
