fd9caab3725f0bf72586148bbc28b7a85c9c531f
younger <1908508603@qq.com>
2022-06-03 23:16:52 +0800

    userinfo

--- go/model/userInfo.go
+++ go/model/userInfo.go
@@ -1,7 +1,18 @@
 package model
 
+import "github.com/jinzhu/gorm"
+
 type UserInfo struct { // 视频发布者信息
-	Id            int64  `json:"id,omitempty"`
+	gorm.Model
+	UserId        int64  `json:"user_id,omitempty"`
+	Name          string `json:"name,omitempty"`
+	FollowCount   int64  `json:"follow_count,omitempty"`
+	FollowerCount int64  `json:"follower_count,omitempty"`
+	IsFollow      bool   `json:"is_follow,omitempty"`
+}
+
+type LoginUser struct {
+	UserId        int64  `json:"user_id,omitempty"`
 	Name          string `json:"name,omitempty"`
 	FollowCount   int64  `json:"follow_count,omitempty"`
 	FollowerCount int64  `json:"follower_count,omitempty"`
