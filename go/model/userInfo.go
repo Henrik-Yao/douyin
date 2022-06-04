140465237de836df04d20e3f1d4d74c04da2e6a7
Hui Yao <77136892+Henrik-Yao@users.noreply.github.com>
2022-06-04 10:55:11 +0800

    Revert "1"

--- go/model/userInfo.go
+++ go/model/userInfo.go
@@ -9,12 +9,3 @@ type Author struct { // 视频发布者信息
 	FollowerCount int64  `json:"follower_count,omitempty"`
 	IsFollow      bool   `json:"is_follow,omitempty"`
 }
-
-type UserInfo struct { // 视频发布者信息
-	Id            int64      `json:"id,omitempty"`
-	Name          string     `json:"name,omitempty"`
-	FollowCount   int64      `json:"follow_count,omitempty"`
-	FollowerCount int64      `json:"follower_count,omitempty"`
-	IsFollow      bool       `json:"is_follow,omitempty"`
-	User          *UserLogin `json:"-"` //用户与密码之间的多对多
-}
--- go/model/userLoginInfo.go
+++ go/model/userLoginInfo.go
@@ -11,12 +11,10 @@ type UserLoginInfo struct {
 	IsFollow      bool   `json:"is_follow,omitempty"`
 }
 
-
 type UserLogin struct {
 	UserId        int64  `json:"user_id,omitempty"`
 	Name          string `json:"name,omitempty"`
 	FollowCount   int64  `json:"follow_count,omitempty"`
 	FollowerCount int64  `json:"follower_count,omitempty"`
 	IsFollow      bool   `json:"is_follow,omitempty"`
-
 }
