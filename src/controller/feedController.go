074809df4576a6d7cc14dad1666a8751a3a4b896
KEPADEDAIDAI <sunabrhh@foxmail.com>
2022-06-05 23:25:07 +0800

    修复int，完成查询视频发布者

--- src/controller/feedController.go
+++ src/controller/feedController.go
@@ -3,7 +3,6 @@ package controller
 import (
 	"douyin/src/common"
 	"douyin/src/middleware"
-	"douyin/src/model"
 	"douyin/src/service"
 	"fmt"
 	"github.com/gin-gonic/gin"
@@ -13,23 +12,30 @@ import (
 )
 
 type FeedVideo struct {
-	Id            int64      `json:"id,omitempty"`
-	Author        model.User `json:"author,omitempty"`
-	PlayUrl       string     `json:"play_url" json:"play_url,omitempty"`
-	CoverUrl      string     `json:"cover_url,omitempty"`
-	FavoriteCount int64      `json:"favorite_count,omitempty"`
-	CommentCount  int64      `json:"comment_count,omitempty"`
-	IsFavorite    bool       `json:"is_favorite,omitempty"`
-	Title         string     `json:"title,omitempty"`
+	Id            uint     `json:"id,omitempty"`
+	Author        FeedUser `json:"author,omitempty"`
+	PlayUrl       string   `json:"play_url" json:"play_url,omitempty"`
+	CoverUrl      string   `json:"cover_url,omitempty"`
+	FavoriteCount uint     `json:"favorite_count,omitempty"`
+	CommentCount  uint     `json:"comment_count,omitempty"`
+	IsFavorite    bool     `json:"is_favorite,omitempty"`
+	Title         string   `json:"title,omitempty"`
 }
 type FeedResponse struct {
 	common.Response
 	VideoList []FeedVideo `json:"video_list,omitempty"`
-	NextTime  int64       `json:"next_time,omitempty"`
+	NextTime  uint        `json:"next_time,omitempty"`
 }
 type FeedNoVideoResponse struct {
 	common.Response
-	NextTime int64 `json:"next_time"`
+	NextTime uint `json:"next_time"`
+}
+type FeedUser struct {
+	Id            uint   `json:"id,omitempty"`
+	Name          string `json:"name,omitempty"`
+	FollowCount   uint   `json:"follow_count,omitempty"`
+	FollowerCount uint   `json:"follower_count,omitempty"`
+	IsFollow      bool   `json:"is_follow,omitempty"`
 }
 
 func Feed(c *gin.Context) {
@@ -42,7 +48,7 @@ func Feed(c *gin.Context) {
 		haveToken = true
 	}
 	var strLastTime = c.Query("latest_time")
-	lastTime, err := strconv.ParseInt(strLastTime, 10, 64)
+	lastTime, err := strconv.ParseInt(strLastTime, 10, 32)
 	if err != nil {
 		lastTime = 0
 	}
@@ -54,9 +60,30 @@ func Feed(c *gin.Context) {
 	var newTime int64 = 0 //返回的视频的最久的一个的时间
 	for _, x := range videoList {
 		var tmp FeedVideo
-		tmp.Id = int64(x.ID)
+		tmp.Id = x.ID
 		tmp.PlayUrl = x.PlayUrl
 		//tmp.Author = //依靠用户信息接口查询
+		var user, err = service.GetUser(x.AuthorId)
+		var feedUser FeedUser
+		if err == nil { //用户存在
+			feedUser.Id = user.ID
+			feedUser.FollowerCount = user.FollowerCount
+			feedUser.FollowCount = user.FollowCount
+			feedUser.Name = user.Name
+			feedUser.IsFollow = false
+			if haveToken {
+				// 查询是否关注
+				tokenStruct, ok := middleware.CheckToken(strToken)
+				if ok && time.Now().Unix() <= tokenStruct.ExpiresAt { //token合法
+					var uid1 = tokenStruct.UserId //用户id
+					var uid2 = x.AuthorId         //视频发布者id
+					if service.IsFollowing(uid1, uid2) {
+						feedUser.IsFollow = true
+					}
+				}
+			}
+		}
+		tmp.Author = feedUser
 		tmp.CommentCount = x.CommentCount
 		tmp.CoverUrl = x.CoverUrl
 		tmp.FavoriteCount = x.FavoriteCount
@@ -65,9 +92,9 @@ func Feed(c *gin.Context) {
 			//查询是否点赞过
 			tokenStruct, ok := middleware.CheckToken(strToken)
 			if ok && time.Now().Unix() <= tokenStruct.ExpiresAt { //token合法
-				var uid = tokenStruct.UserId              //用户id
-				var vid = x.ID                            // 视频id
-				if service.CheckFavorite(uid, int(vid)) { //有点赞记录
+				var uid = tokenStruct.UserId         //用户id
+				var vid = x.ID                       // 视频id
+				if service.CheckFavorite(uid, vid) { //有点赞记录
 					tmp.IsFavorite = true
 				}
 			}
@@ -80,7 +107,7 @@ func Feed(c *gin.Context) {
 		c.JSON(http.StatusOK, FeedResponse{
 			Response:  common.Response{StatusCode: 0}, //成功
 			VideoList: feedVideoList,
-			NextTime:  newTime,
+			NextTime:  uint(newTime),
 		})
 	} else {
 		c.JSON(http.StatusOK, FeedNoVideoResponse{
