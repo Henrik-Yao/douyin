b3d391378e048f306968db9937b7c60978eb9fa1
robincai <40911829+robincaicn@users.noreply.github.com>
2022-06-05 17:04:45 +0800

    Update commentService.go

--- src/service/commentService.go
+++ src/service/commentService.go
@@ -8,7 +8,7 @@ import (
 
 func GetCommentList(videoId string) ([]model.Comment, error) {
 	var commentList []model.Comment
-	if err := dao.SqlSession.Table("comment").Where("video_id=? and deleted_at is null", videoId).Find(&commentList).Error; err != nil { //找不到记录
+	if err := dao.SqlSession.Table("comment").Where("video_id=? and deleted_at is null", videoId).Find(&commentList).Error; err != nil {
 		return commentList, err
 	}
 	return commentList, nil
