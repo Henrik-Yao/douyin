499d50a12916503b47d35cda9d56607b37f8da43
younger <1908508603@qq.com>
2022-06-06 00:33:28 +0800

    young

--- src/controller/publishController.go
+++ src/controller/publishController.go
@@ -7,42 +7,49 @@ package controller
 import (
 	"douyin/src/common"
 	"douyin/src/dao"
-	"douyin/src/middleware"
 	"douyin/src/model"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	"net/http"
 	"path/filepath"
-	"time"
+	"strconv"
 )
+
+type ReturnAuthor struct {
+	AuthorId      uint   `json:"author_id"`
+	Name          string `json:"name"`
+	FollowCount   uint   `json:"follow_count"`
+	FollowerCount uint   `json:"follower_count"`
+	IsFollow      bool   `json:"is_follow"`
+}
+
+type ReturnVideo struct {
+	VideoId       uint         `json:"video_id"`
+	Author        ReturnAuthor `json:"author"`
+	PlayUrl       string       `json:"play_url"`
+	CoverUrl      string       `json:"cover_url"`
+	FavoriteCount uint         `json:"favorite_count"`
+	CommentCount  uint         `json:"comment_count"`
+	IsFavorite    bool         `json:"is_favorite"`
+	Title         string       `json:"title"`
+}
 
 type VideoListResponse struct {
 	common.Response
-	VideoList []model.Video `json:"video_list"`
+	VideoList []ReturnVideo `json:"video_list"`
 }
 
-//上传视频方法
+////上传视频方法
 func Publish(c *gin.Context) {
-	//1.验证token
-	token := c.PostForm("token")
-	tokenStruck, ok := middleware.CheckToken(token)
-	if !ok {
-		c.JSON(http.StatusOK, gin.H{"code": 403, "msg": "token不正确"})
-		c.Abort() //阻止执行
-		return
+	//1.中间件验证token后，获取userId
+	getUserId, _ := c.Get("user_id")
+	var userId uint
+	if v, ok := getUserId.(uint); ok {
+		userId = v
 	}
-	//token超时
-	if time.Now().Unix() > tokenStruck.ExpiresAt {
-		c.JSON(http.StatusOK, gin.H{"code": 402, "msg": "token过期"})
-		c.Abort() //阻止执行
-		return
-	}
-
-	//获取id 和 name
-	user_id := tokenStruck.UserId
-
-	//2.接收传来的数据
+	//2.接收请求参数信息
+	title := c.PostForm("title") //获取传入的标题
 	data, err := c.FormFile("data")
 	if err != nil {
 		c.JSON(http.StatusOK, common.Response{
@@ -51,10 +58,9 @@ func Publish(c *gin.Context) {
 		})
 		return
 	}
-
-	//2.视频存入本地，列表信息存入数据库
+	//3.返回至前端页面的展示信息
 	filename := filepath.Base(data.Filename)
-	finalName := fmt.Sprintf("%d_%s", user_id, filename)
+	finalName := fmt.Sprintf("%d_%s", userId, filename)
 	saveFile := filepath.Join("./videos/", finalName)
 	if err := c.SaveUploadedFile(data, saveFile); err != nil {
 		c.JSON(http.StatusOK, common.Response{
@@ -67,77 +73,74 @@ func Publish(c *gin.Context) {
 		StatusCode: 0,
 		StatusMsg:  finalName + "--上传成功",
 	})
+	var playUrl string
+	playUrl = "http://192.168.148.246:8000/" + "videos/" + finalName
+	var coverUrl string
+	coverUrl = "https://cdn.pixabay.com/photo/2016/03/27/18/10/bear-1283347_1280.jpg"
 
-	//3.建数据库表
-	//dao.SqlSession.AutoMigrate(&model.UserLoginInfo{})
-	dao.SqlSession.AutoMigrate(&model.VideoNoAuthor{}) //模型关联到数据库表
-
-	title := c.PostForm("title") //获取传入的标题
-	//保存视频相关信息
-	videoNoAuthor := model.VideoNoAuthor{
+	//4.保存发布信息至数据库,刚开始发布，喜爱和评论默认为0
+	video := model.Video{
 		Model:         gorm.Model{},
-		UserId:        int64(user_id),
-		PlayUrl:       "../videos/" + finalName,
-		CoverUrl:      "......",
+		AuthorId:      userId,
+		PlayUrl:       playUrl,
+		CoverUrl:      coverUrl,
 		FavoriteCount: 0,
 		CommentCount:  0,
-		IsFavorite:    false,
 		Title:         title,
 	}
-	dao.SqlSession.Table("video_no_authors").Create(&videoNoAuthor)
-
+	dao.SqlSession.Table("videos").Create(&video)
 }
 
 //////获取列表的方法
 func PublishList(c *gin.Context) {
 	//1.中间件鉴权token
-
 	//2.查询当前id用户的所有视频，返回页面
-	userId := c.Query("user_id")
-	//根据id查找用户
-	var userLoginInfo model.UserLoginInfo
-	dao.SqlSession.Table("user_login_infos").Where("user_id=?", userId).First(&userLoginInfo)
-	loginUser := model.UserLogin{
-		UserId:        userLoginInfo.UserId,
-		Name:          userLoginInfo.Name,
-		FollowCount:   userLoginInfo.FollowCount,
-		FollowerCount: userLoginInfo.FollowerCount,
-		IsFollow:      userLoginInfo.IsFollow,
+	getUserId := c.Query("user_id")
+	userId, _ := strconv.Atoi(getUserId)
+
+	//根据用户id查找用户
+	var getUser model.User
+	dao.SqlSession.Table("users").Where("id=?", userId).First(&getUser)
+	returnAuthor := ReturnAuthor{
+		AuthorId:      uint(userId),
+		Name:          getUser.Name,
+		FollowCount:   getUser.FollowCount,
+		FollowerCount: getUser.FollowerCount,
+		IsFollow:      false, //需要调用其他人接口拿到
 	}
 	//根据用户id查找 所有相关视频信息
-	var videoNoAuthorList []model.VideoNoAuthor
-	dao.SqlSession.Table("video_no_authors").Where("user_id=?", userId).Find(&videoNoAuthorList)
-
-	if len(videoNoAuthorList) == 0 {
+	var videoList []model.Video
+	dao.SqlSession.Table("videos").Where("author_id=?", userId).Find(&videoList)
+	if len(videoList) == 0 {
 		c.JSON(http.StatusOK, VideoListResponse{
 			Response: common.Response{
 				StatusCode: 1,
-				StatusMsg:  "No query found.",
+				StatusMsg:  "null",
 			},
 			VideoList: nil,
 		})
 	} else {
 		//需要展示的列表信息
-		var videoList []model.Video
-		for i := 0; i < len(videoNoAuthorList); i++ {
-			video := model.Video{
-				VideoId:       int64(videoNoAuthorList[i].ID),
-				Author:        loginUser,
-				PlayUrl:       videoNoAuthorList[i].PlayUrl,
-				CoverUrl:      videoNoAuthorList[i].CoverUrl,
-				FavoriteCount: videoNoAuthorList[i].FavoriteCount,
-				CommentCount:  videoNoAuthorList[i].CommentCount,
-				IsFavorite:    videoNoAuthorList[i].IsFavorite,
-				Title:         videoNoAuthorList[i].Title,
+		var returnVideoList []ReturnVideo
+		for i := 0; i < len(videoList); i++ {
+			returnVideo := ReturnVideo{
+				VideoId:       uint(videoList[i].ID),
+				Author:        returnAuthor,
+				PlayUrl:       videoList[i].PlayUrl, //
+				CoverUrl:      videoList[i].CoverUrl,
+				FavoriteCount: videoList[i].FavoriteCount,
+				CommentCount:  videoList[i].CommentCount,
+				IsFavorite:    false,
+				Title:         videoList[i].Title,
 			}
-			videoList = append(videoList, video)
+			returnVideoList = append(returnVideoList, returnVideo)
 		}
 		c.JSON(http.StatusOK, VideoListResponse{
 			Response: common.Response{
 				StatusCode: 0,
 				StatusMsg:  "success",
 			},
-			VideoList: videoList,
+			VideoList: returnVideoList,
 		})
 	}
 }
