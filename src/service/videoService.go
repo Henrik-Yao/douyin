49069cdeac26fa99cd955d0f018003e76777c03b
KEPADEDAIDAI <sunabrhh@foxmail.com>
2022-06-05 14:44:10 +0800

    feed可以返回所有视频数据

--- src/service/videoService.go
+++ src/service/videoService.go
@@ -1,1 +1,18 @@
 package service
+
+import (
+	"douyin/src/dao"
+	"douyin/src/model"
+	"fmt"
+)
+
+//获得视频列表
+func FeedGet() ([]model.Video, error) {
+	result := dao.SqlSession.Table("videos")
+	fmt.Println("查询所有视频信息")
+	fmt.Println(result.Error)
+	var VideoList []model.Video
+	VideoList = make([]model.Video, 0)
+	err := dao.SqlSession.Table("videos").Find(&VideoList).Error
+	return VideoList, err
+}
