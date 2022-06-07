8367b876b298ce7f57053c7e222271e177e90995
younger <1908508603@qq.com>
2022-06-07 13:26:46 +0800

    modify

--- src/controller/publishController.go
+++ src/controller/publishController.go
@@ -36,8 +36,7 @@ type VideoListResponse struct {
 	VideoList []ReturnVideo `json:"video_list"`
 }
 
-////上传视频方法
-func Publish(c *gin.Context) {
+func Publish(c *gin.Context) { //上传视频方法
 	//1.中间件验证token后，获取userId
 	getUserId, _ := c.Get("user_id")
 	var userId uint
@@ -45,7 +44,7 @@ func Publish(c *gin.Context) {
 		userId = v
 	}
 	//2.接收请求参数信息
-	title := c.PostForm("title") //获取传入的标题
+	title := c.PostForm("title")
 	data, err := c.FormFile("data")
 	if err != nil {
 		c.JSON(http.StatusOK, common.Response{
@@ -58,7 +57,7 @@ func Publish(c *gin.Context) {
 	//3.返回至前端页面的展示信息
 	filename := filepath.Base(data.Filename)
 	finalName := fmt.Sprintf("%d_%s", userId, filename)
-	saveFile := filepath.Join("./videos/", finalName)
+	saveFile := filepath.Join("resources/videos/", finalName)
 	if err := c.SaveUploadedFile(data, saveFile); err != nil {
 		c.JSON(http.StatusOK, common.Response{
 			StatusCode: 1,
@@ -68,10 +67,12 @@ func Publish(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, common.Response{
 		StatusCode: 0,
-		StatusMsg:  finalName + "--上传成功",
+		StatusMsg:  finalName + "--uploaded successfully",
 	})
+	//尝试用远程服务器但未部署成功，此时用本地静态资源服务器
 	var playUrl string
 	playUrl = "http://192.168.148.246:8000/" + "videos/" + finalName
+	//封面url已写死
 	var coverUrl string
 	coverUrl = "https://cdn.pixabay.com/photo/2016/03/27/18/10/bear-1283347_1280.jpg"
 
@@ -88,8 +89,7 @@ func Publish(c *gin.Context) {
 	service.CreateVideo(&video)
 }
 
-//////获取列表的方法
-func PublishList(c *gin.Context) {
+func PublishList(c *gin.Context) { //获取列表的方法
 	//1.中间件鉴权token
 	getHostId, _ := c.Get("user_id")
 	var HostId uint
@@ -105,8 +105,8 @@ func PublishList(c *gin.Context) {
 	getUser, err := service.GetUser(GuestId)
 	if err != nil {
 		c.JSON(http.StatusOK, common.Response{
-			StatusCode: 403,
-			StatusMsg:  "找不到作者！",
+			StatusCode: 1,
+			StatusMsg:  "Not find this person.",
 		})
 		c.Abort()
 		return
@@ -117,7 +117,7 @@ func PublishList(c *gin.Context) {
 		Name:          getUser.Name,
 		FollowCount:   getUser.FollowCount,
 		FollowerCount: getUser.FollowerCount,
-		IsFollow:      service.IsFollowing(HostId, GuestId), //需要调用其他人接口拿到
+		IsFollow:      service.IsFollowing(HostId, GuestId),
 	}
 	//根据用户id查找 所有相关视频信息
 	var videoList []model.Video
@@ -130,14 +130,13 @@ func PublishList(c *gin.Context) {
 			},
 			VideoList: nil,
 		})
-	} else {
-		//需要展示的列表信息
+	} else { //需要展示的列表信息
 		var returnVideoList []ReturnVideo
 		for i := 0; i < len(videoList); i++ {
 			returnVideo := ReturnVideo{
 				VideoId:       videoList[i].ID,
 				Author:        returnAuthor,
-				PlayUrl:       videoList[i].PlayUrl, //
+				PlayUrl:       videoList[i].PlayUrl,
 				CoverUrl:      videoList[i].CoverUrl,
 				FavoriteCount: videoList[i].FavoriteCount,
 				CommentCount:  videoList[i].CommentCount,
