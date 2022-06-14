71abc1f60b8a33ae5653247a1f8d7c34f4bcf8a7
younger <1908508603@qq.com>
2022-06-15 00:03:21 +0800

    1

--- src/dao/cos.go
+++ src/dao/cos.go
@@ -4,7 +4,7 @@ const (
 	COS_BUCKET_NAME = "dong"
 	COS_APP_ID      = "1305843950"
 	COS_REGION      = "ap-nanjing"
-	COS_SECRET_ID   = "AKIDa0B5j6C1ZMvDG4brqZ1B5i5BzXprc6KH"
-	COS_SECRET_KEY  = "h7G9l6AxTigNozuYuzoMfjX4NREl1KNA"
+	COS_SECRET_ID   = "AKID1rRmqdAQP7ZjcGjvqUqwGK7eusZz92ey"
+	COS_SECRET_KEY  = "5tQhjUyB7nBI8i13oIVR5SYOxFH2WjfK"
 	COS_URL_FORMAT  = "http://%s-%s.cos.%s.myqcloud.com"
 )
